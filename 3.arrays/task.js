9ea949490cf52073d0a54501dab1495f91382153
Alex77 <fktrctq321@gmail.com>
2023-01-10 22:26:07 +0700

    -3-

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,7 @@
 function compareArrays(arr1, arr2) {
-    
-    return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])
+      return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])
 }
    
-
 compareArrays([8, 9], [6]) // false, разные значения
 compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]) // false, разные значения
 compareArrays([9, 2, 4, 8, 2], [9, 2, 4]) // false, разные значения
