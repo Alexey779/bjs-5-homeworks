0fbff9632a24d60fb9f2cf46d71803ba92ee687a
Alex77 <fktrctq321@gmail.com>
2023-01-18 10:39:06 +0700

    Revert "objects1"
    This reverts commit 6f74e89db5316738cc19bc0be17bc6bd2c2d7486.

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,1 +1,45 @@
 
+function Student(name, gender, age, ...marks) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = marks;
+    this.marks = [];
+}
+
+Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+}
+
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.addMarks.hasOwnProperty('marks') !== undefined) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd)
+    } else {
+        console.log('Студент отчислен');
+    }
+}
+
+Student.prototype.getAverage = function () {
+    if (this.marks === undefined || (this.marks.length < 1)) {
+        return 0
+    } else {
+        let sum = 0;
+        for (let i = 0; i < this.marks.length; i++) {
+            sum += this.marks[i];
+        }
+        return sum / this.marks.length
+    }
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+    }
+    return sum / this.marks.length
+}
+
+Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+}
+}
